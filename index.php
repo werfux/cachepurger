b00868a3ae1093b394c6486e54d84a426d617c8d
Thilo Habenreich <thabenreich@chip.de>
2017-11-13 16:15:39 +0100

    Added requirements check

--- index.php
+++ index.php
@@ -2,12 +2,23 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+use BC\Purger\Helper\RequirementsHelper;
 use BC\Purger\Command\AkamaiPurgeCommand;
 use BC\Purger\Command\AkamaiCredentialsCommand;
 use BC\Purger\Command\VarnishPurgeCommand;
 use BC\Purger\Command\RedisPurgeCommand;
 use Symfony\Component\Console\Application;
 
+// Requirements Check
+try {
+    RequirementsHelper::checkMinimumPHPVersion();
+    RequirementsHelper::checkRequiredPHPExtensions();
+} catch (\Exception $exception) {
+    printf($exception->getMessage() . "\n");
+    die();
+}
+
+// Application
 $consoleApplication = new Application();
 
 // Bootstrap Application
